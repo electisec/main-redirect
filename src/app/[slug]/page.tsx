9038c4bd80c42e7b86ed40fb96f6f51c13900e36
nullity <70228821+nullity00@users.noreply.github.com>
2025-11-20 14:31:23 +0530

    yaudit

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,23 +1,18 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function RedirectPage() {
   const pathname = usePathname();
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const slug = pathname;
       if (!slug) return;
-      window.location.href = `https://electisec.com${slug}`;
+      window.location.href = `https://yaudit.dev${slug}`;
     }
   }, [pathname]);
 
-  return (
-    <>
-    </>
-  );
+  return <></>;
 }
-
-
